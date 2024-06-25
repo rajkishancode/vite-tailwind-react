aca1f98c66105877b2606e16a176d629dc36f463
Raj Kishan <68591610+rajkishancode@users.noreply.github.com>
2024-06-25 20:47:31 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl text-red-500 text-center">
+        Welcome to vite with tailwind css and React
+      </h1>
+    </>
+  );
+}
+
+export default App;
